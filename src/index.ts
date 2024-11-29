4c54d74eeca447849828c586beccc1086b5b8c70
Iulian Safta <iulian@olyn.com>
2024-11-29 16:16:16 +0100

    rollback

--- src/index.ts
+++ src/index.ts
@@ -17,29 +17,20 @@ async function run(): Promise<void> {
     const prNumber = payload.pull_request?.number;
     const action = payload.action;
     const branchName = payload.pull_request?.head.ref || '';
-
-    core.info(`Action: ${action}`);
-    core.info(`branchName: ${branchName}`)
     
     if (!prNumber) {
       throw new Error('No PR number found in context');
     }
 
-    if (!branchName) {
-      throw new Error('No branchName found in context');
-    }
-
-   const sanitizedBranch = branchName
+    const sanitizedBranch = branchName
       .toLowerCase()
-      .replace(/[^a-z0-9-/]/g, '-'); 
+      .replace(/[^a-z0-9-/]/g, '-');
 
     const subdomain = `pr-${prNumber}-${sanitizedBranch}`;
     const fullDomain = `${subdomain}.${baseDomain}`;
 
-    core.info(`Processing PR #${prNumber} for ${fullDomain}`);
-
-    if (action === 'opened' || action === 'reopened' || action === 'ready_for_review' || action === 'converted_to_draft') {
-      await cf.dns.records.create({
+    if (action === 'opened' || action === 'reopened') {
+      await cf.dns.records.create( {
         zone_id: zoneId,
         type: 'CNAME',
         name: subdomain,
